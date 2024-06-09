c3deecf12d91ba849848cff40835a9501f9e822e
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-10 01:43:13 +0300

    adding basic UI

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("OMG")
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Chat with handbooks", page_icon=":books:")
+    st.header("chat with handbooks :books:")
+    st.text_input("Ask a question about the handbook:")
+
+    with st.sidebar:
+        st.subheader("handbook")
+        st.file_uploader("Upload a handbook")
+        st.button("Process")
+
+
+if __name__ == "__main__":
+    main()
