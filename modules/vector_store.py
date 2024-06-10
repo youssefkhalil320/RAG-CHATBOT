644a73d27f2b0fb57ab38d69f806b320a5668453
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-10 16:03:44 +0300

    adding vector store

---
+++ modules/vector_store.py
@@ -0,0 +1,10 @@
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+
+    return vectorstore
