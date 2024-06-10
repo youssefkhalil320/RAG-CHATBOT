08de36e2566ff8c9fb0c533005bb67cc802a58e0
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-10 16:18:32 +0300

    adding session state

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from dotenv import load_dotenv
 from modules.pdf_processing import get_pdf_text
 from modules.text_chunking import get_text_chunks
 from modules.vector_store import get_vectorstore
+from modules.conversation import get_conversational_chain
 
 
 def main():
     load_dotenv()
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.set_page_config(page_title="Chat with handbooks", page_icon=":books:")
     st.header("chat with handbooks :books:")
@@ -22,6 +26,8 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
                 vector_store = get_vectorstore(text_chunks)
+                st.session_state.conversation = get_conversational_chain(
+                    vector_store)
 
 
 if __name__ == "__main__":
